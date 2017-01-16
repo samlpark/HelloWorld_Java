b98876cbb042fc78aa1c5a828b885937320947fa
박찬준 <pcj9000@gmail.com>
2017-01-16 09:13:32 +0900

    something's changed

--- VariableExam/src/com/test_variable_exam/VariableExam.java
+++ VariableExam/src/com/test_variable_exam/VariableExam.java
@@ -4,9 +4,7 @@ public class VariableExam
 {
 	public static void main(String args[])
 	{
-		int value = 10;
-		int result = value + 10;
-		System.out.println(result);
+		
+		System.out.println("Hello.");
 	}
-
 }
